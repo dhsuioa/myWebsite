9640e54f3ba6175db8fa81384a8bf207bb4e71b6
Alexandr Litvinenko <a27litvinenko@gmail.com>
2024-07-02 16:10:56 +0300

    publicPath

--- quasar.config.js
+++ quasar.config.js
@@ -61,7 +61,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/myWebsite',
       // analyze: true,
       // env: {},
       // rawDefine: {}
