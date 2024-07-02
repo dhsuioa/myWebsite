7b871cca903b920426ff98a06a128e2fa1511f55
Alexandr Litvinenko <a27litvinenko@gmail.com>
2024-07-02 15:42:53 +0300

    works component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,6 @@
 <template>
   <!-- <q-page class="row items-center justify-evenly"> -->
-  <q-page>
-    <!-- <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component> -->
+  <q-page class="row justify-center">
     <contacts/>
     <work/>
   </q-page>
@@ -15,33 +9,4 @@
 <script setup lang="ts">
 import Contacts from 'components/Contacts.vue';
 import Work from 'components/Work.vue';
-// import { Todo, Meta } from 'components/models';
-// import ExampleComponent from 'components/ExampleComponent.vue';
-// import { ref } from 'vue';
-
-// const todos = ref<Todo[]>([
-//   {
-//     id: 1,
-//     content: 'ct1'
-//   },
-//   {
-//     id: 2,
-//     content: 'ct2'
-//   },
-//   {
-//     id: 3,
-//     content: 'ct3'
-//   },
-//   {
-//     id: 4,
-//     content: 'ct4'
-//   },
-//   {
-//     id: 5,
-//     content: 'ct5'
-//   }
-// ]);
-// const meta = ref<Meta>({
-//   totalCount: 1200
-// });
 </script>
