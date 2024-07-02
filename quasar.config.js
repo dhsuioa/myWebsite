0e1691a4048a5bb391d8e92fe75dbf0aace2a822
Alexandr Litvinenko <a27litvinenko@gmail.com>
2024-07-02 16:01:43 +0300

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -30,7 +30,7 @@ module.exports = configure(function (/* ctx */) {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: [
       'app.scss',
-      'src/css/jetbrains-mono.css'
+      'jetbrains-mono.css'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
