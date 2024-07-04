0b81215763a017b19709eecb867d5985e308dc45
Alexandr Litvinenko <a27litvinenko@gmail.com>
2024-07-04 20:44:30 +0300

    loading

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
-  <router-view />
+  <div id="app">
+    <div v-if="isLoading" class="fullscreen flex flex-center">
+      <q-spinner-grid color="primary" size="3em" />
+    </div>
+    <router-view v-show="!isLoading" />
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+import { ref, onMounted } from 'vue'
 
+const isLoading = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false
+  }, 1000)
+})
 </script>
+
+<style>
+</style>
